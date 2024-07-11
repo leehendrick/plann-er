9f24542a1874db1d85407ff6a185d5be3fc5d5e8
leehendrick <imhendrick16@gmail.com>
2024-07-11 22:39:14 +0100

    add input element

--- plann.er/src/app.tsx
+++ plann.er/src/app.tsx
@@ -3,11 +3,14 @@ export function App() {
 
   return (
       <div className="h-screen flex items-center justify-center">
-          <div className=" text-center max-w-3xl px-6">
+          <div className="max-w-3xl w-full text-center px-6 space-y-10">
               <p className="text-zinc-300 text-lg">Convide seus amigos e planeje a sua próxima viagem!</p>
 
-              <div>
+              <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+                  <input type="text" placeholder="Para onde você vai?"/>
+                  <input type="text" placeholder="Quando?"/>
 
+                  <button>Continuar</button>
               </div>
 
               <p className="text-sm text-zinc-500">
