27694c10d215e981bef7d13e583f1345fd292f3d
leehendrick <imhendrick16@gmail.com>
2024-07-11 22:23:10 +0100

    add the upper text and the footer text

--- plann.er/src/app.tsx
+++ plann.er/src/app.tsx
@@ -2,8 +2,20 @@
 export function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello, World!
-    </h1>
+      <div className="h-screen flex items-center justify-center">
+          <div className=" text-center max-w-3xl px-6">
+              <p className="text-zinc-300 text-lg">Convide seus amigos e planeje a sua próxima viagem!</p>
+
+              <div>
+
+              </div>
+
+              <p className="text-sm text-zinc-500">
+                  Ao planejar sua viagem pela plann.er você automaticamente concorda <br/> com nossos
+                    <a className="text-zinc-300 underline" href="#"> termos de uso</a> e
+                    <a className="text-zinc-300 underline" href="#"> políticas de privacidade</a>.
+              </p>
+          </div>
+      </div>
   )
 }
